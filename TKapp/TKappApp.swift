12f43a8370364f6620cccd4f8f0756de04e7c0fb
YasseenR <yasseenr@gmail.com>
2024-06-12 23:32:25 -0400

    Initial Commit

---
+++ TKapp/TKappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKappApp.swift
+//  TKapp
+//
+//  Created by Yasseen Rouni on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TKappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
